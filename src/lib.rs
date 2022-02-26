8a2857062836322a4f86fc04c1199b061435d7f6
Ari Seyhun <ariseyhun@live.com.au>
2022-02-27 03:39:43 +0700

    feat: add world colliders and cubes demo

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,32 @@
-pub mod assets;
-pub mod player;
-pub mod ui;
-pub mod world;
+use assets::AssetsPlugin;
+use bevy::{diagnostic::FrameTimeDiagnosticsPlugin, prelude::*};
+use bevy_egui::EguiPlugin;
+use bevy_polyline::PolylinePlugin;
+use heron::{Gravity, PhysicsPlugin};
+use player::PlayerPlugin;
+use ui::UiPlugin;
+use world::WorldPlugin;
+
+mod assets;
+mod player;
+mod ui;
+mod world;
+
+pub struct GamePlugin;
+
+impl Plugin for GamePlugin {
+    fn build(&self, app: &mut App) {
+        app
+            // External plugins
+            .add_plugin(EguiPlugin)
+            .add_plugin(FrameTimeDiagnosticsPlugin::default())
+            .add_plugin(PolylinePlugin)
+            .add_plugin(PhysicsPlugin::default())
+            .insert_resource(Gravity::from(Vec3::new(0.0, -9.81, 0.0)))
+            // Game plugins
+            .add_plugin(AssetsPlugin)
+            .add_plugin(PlayerPlugin)
+            .add_plugin(UiPlugin)
+            .add_plugin(WorldPlugin);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,15 @@
 use bevy::{
-    diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin},
     prelude::*,
     window::{WindowMode, WindowResizeConstraints},
 };
-use bevy_egui::EguiPlugin;
-use bevy_polyline::PolylinePlugin;
-use game::{assets::AssetsPlugin, player::PlayerPlugin, ui::UiPlugin, world::WorldPlugin};
-use heron::PhysicsPlugin;
+use game::GamePlugin;
 
 fn main() {
     App::new()
-        // External plugins
         .insert_resource(WindowDescriptor {
             title: "app".to_string(),
-            width: 1280.0,
-            height: 720.0,
+            width: 1920.0,
+            height: 1080.0,
             position: None,
             resize_constraints: WindowResizeConstraints {
                 min_width: 256.0,
@@ -34,15 +29,6 @@ fn main() {
         })
         .insert_resource(Msaa { samples: 4 })
         .add_plugins(DefaultPlugins)
-        .add_plugin(EguiPlugin)
-        .add_plugin(FrameTimeDiagnosticsPlugin::default())
-        .add_plugin(LogDiagnosticsPlugin::default())
-        .add_plugin(PolylinePlugin)
-        .add_plugin(PhysicsPlugin::default())
-        // Game plugins
-        .add_plugin(AssetsPlugin)
-        .add_plugin(PlayerPlugin)
-        .add_plugin(UiPlugin)
-        .add_plugin(WorldPlugin)
+        .add_plugin(GamePlugin)
         .run();
 }
